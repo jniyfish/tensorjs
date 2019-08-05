e857f701fc570c9b0b6dbc0b26b8a1a307692d50
jniyfish <39803423+jniyfish@users.noreply.github.com>
2019-08-05 11:40:23 +0800

    Add files via upload

--- predict.js
+++ predict.js
@@ -1,10 +1,10 @@
-$("#image-selector").change(function(){
+$("#image-selector").change(function () {
     let reader = new FileReader();
-    reader.onload = function()
-    {
+    reader.onload = function () {
         let dataURL = reader.result;
-        $('#selected-image').attr("src",dataURL);
+        $('#selected-image').attr("src", dataURL);
         $("#prediction-list").empty();
+        $("#mysvg").empty();
     }
     let file = $("#image-selector").prop('files')[0];
     reader.readAsDataURL(file);
@@ -14,42 +14,227 @@ $("#image-selector").change(function(){
 
 
 let mode;
-(async function(){
+(async function () {
     console.log("loading");
-    model =await tf.loadModel('http://localhost:81/tfjs-models/model/model.json');
+    model = await tf.loadModel('http://localhost:81/tfjs-models/model/model.json');
     $('.progress-bar').hide();
 })();
 
-$("#predict-button").click(async function(){
+$("#histogram-button").click(async function () {
+
+    let ksize = new cv.Size(5, 5);
+
+    let img = await cv.imread('selected-image');
+    let src = await cv.imread('selected-image');
+
+
+    let img_gray = new cv.Mat();
+    cv.cvtColor(img, img_gray, cv.COLOR_BGR2GRAY, dstCn = 0)
+
+    let img_blur = new cv.Mat();
+    cv.blur(img, img_blur, ksize, anchor = new cv.Point(-1, -1), borderType = cv.BORDER_DEFAULT)
+
+    let img_blur_gray = new cv.Mat();
+    cv.cvtColor(img_blur, img_blur_gray, cv.COLOR_BGR2GRAY, dstCn = 0)
+
+    console.log('image width: ' + src.cols + '\n' +
+        'image height: ' + src.rows + '\n' +
+        'image size: ' + src.size().width + '*' + src.size().height + '\n' +
+        'image depth: ' + src.depth() + '\n' +
+        'image channels ' + src.channels() + '\n' +
+        'image type: ' + src.type() + '\n');
+
+    cv.imshow('canvasOutput', img_gray);
+    const b = tf.scalar(512);
+
+    let image = $('#canvasOutput').get(0);
+    let tensor_img_gray = tf.fromPixels(image, 1)
+        .flatten()
+        .toFloat()
+        .expandDims(0);
+
+    console.log(tensor_img_gray);
+
+    cv.imshow('canvasOutput', img_blur_gray);
+
+    image = $('#canvasOutput').get(0);
+    let tensor_img_blur_gray = tf.fromPixels(image, 1)
+        .flatten()
+        .toFloat()
+        .expandDims(0);
+
+    tensor_img_blur_gray = tensor_img_blur_gray.div(b);
+    tensor_img_gray = tensor_img_gray.div(b);
+
+
+    tensor_img_gray = tensor_img_gray.sub(tensor_img_blur_gray);
+    //tensor_img_gray = tensor_img_gray.flatten();
+    //tensor_img_gray = tensor_img_gray.div(b);
+
+    const values = tensor_img_gray.dataSync();
+    const dataset = Array.from(values);
+    console.log(dataset);
+    ////////
+    var width = 480;
+    var height = 480;
+    var padding = {
+        top: 0,
+        right: 25,
+        bottom: 52.5,
+        left: 25
+    };
+
+    var svg = d3.select("body").append("svg")
+        //.attr('style', 'background-color:white')
+        .attr("id","mysvg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var binNum = 512,
+        rangeMin = -0.5,
+        rangeMax = 0.5;
+
+    var histogram = d3.layout.histogram()
+        .range([rangeMin, rangeMax])
+        .bins(binNum)
+        .frequency(false);
+
+    var hisData = histogram(dataset);
+
+    console.log(hisData);
+
+    var xAxisWidth = width - padding.left - padding.right
+    var yAxisWidth = 480
+    var xTicks = hisData.map(function (d) {
+        return d.x
+    }) // ???layout?????????x
+
+    var xScale = d3.scale.ordinal() // ?????
+        .domain(xTicks) // ???
+        .rangeBands([0, xAxisWidth]) // ??
+
+    var yScale = d3.scale.linear() // ?????
+        .domain([0,0.07])
+        .range([0.1, yAxisWidth]) // ??
+
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+
+    svg.append("g")
+        .attr("transform", "translate(" + padding.left + "," + (height - padding.bottom) + ")")
+
+    var gRect = svg.append("g")
+        .attr("transform", "translate(" + padding.left + "," + (-padding.bottom) + ")")
+
+    gRect.selectAll("rect")
+        .data(hisData)
+        .enter()
+        .append("rect")
+        .attr("class", "rect")
+        .style("fill", "#ff0000")
+        .attr("x", function (d, i) {
+            return xScale(d.x)
+        })
+        .attr("y", function (d, i) {
+            return height - yScale(d.y)
+        })
+        .attr("width", function (d, i) {
+            return xScale.rangeBand()
+        })
+        .attr("height", function (d) {
+            return yScale(d.y)
+        }
+        )
+
+    ///////////////////////////
+    gRect.style("opacity",0.0);
+
+    var lineGenerator = d3.svg.line()
+							.x(function(d){ return xScale(d.x); })
+							.y(function(d){ return height - yScale(d.y); })
+							.interpolate("basis");
+	
+	var gLine = svg.append("g")
+                    .attr("transform","translate(" + padding.left + "," + ( -padding.bottom ) +  ")")
+                    .style("color","red")
+                    .style("fill", "#ff0000")
+					.style("opacity",1);
+	
+	gLine.append("path")
+		.attr("class","linePath")
+		.attr("d",lineGenerator(hisData));
+
+
+
+
+});
+
+$("#predict-button").click(async function () {
     let image = $('#selected-image').get(0);
     console.log(image);
-    let tensor = tf.fromPixels(image,1)
-        .resizeNearestNeighbor([480,480])   
+    let tensor = tf.fromPixels(image, 1)
+       // .resizeNearestNeighbor([480, 480])
         .toFloat()
         .expandDims(0);
 
     const b = tf.scalar(255);
 
+    const values = tensor.dataSync();
+    const dataset = Array.from(values);
+    console.log(dataset);
     tensor = tensor.div(b);
-        //preproces to be add
+    //preproces to be add
 
-    let predictions = await model.predict(tensor).data(); 
-    console.log(predictions);  //tensor is predict target matrix
+    let predictions = await model.predict(tensor).data();
+    console.log(predictions); //tensor is predict target matrix
 
     let top = Array.from(predictions)
-        .map(function(p,i){
-            return{
+        .map(function (p, i) {
+            return {
                 probability: p,
-                className:IMAGENET_CLASSES[i]
+                className: IMAGENET_CLASSES[i]
             };
-        }).sort(function(a,b){
+        }).sort(function (a, b) {
             return b.probability - a.probability;
-        }).slice(0,5);
-    
+        }).slice(0, 5);
+
     $("#prediction-list").empty();
     top.forEach(function (p) {
         $('#prediction-list').append(`<li>${p.className}: ${p.probability.toFixed(7)}</li>`);
     });
-    
+
 });
 
+///////////
+$("#save").click(async function () {
+    $(".svg").empty();
+    var svg = document.querySelector("svg");
+    var rect = document.querySelector("rect")
+    var svgData = new XMLSerializer().serializeToString(svg);
+    var canvas = document.createElement("canvas");
+    var svgSize = svg.getBoundingClientRect();
+    canvas.width = svgSize.width * 1;
+    canvas.height = svgSize.height * 1;
+    canvas.style.width = svgSize.width;
+    canvas.style.height = svgSize.height;
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.scale(1, 1);
+    var img = document.createElement("img");
+    img.setAttribute("src", "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData))));
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0,480,480);
+     
+      var canvasdata = canvas.toDataURL("image/jpg", 1);
+  
+      var pngimg = '<img src="' + canvasdata + '">';
+      d3.select("#pngdataurl").html(pngimg);
+  
+      var a = document.createElement("a");
+      a.download = "histogram" + ".jpg";
+      a.href = canvasdata;
+      document.body.appendChild(a);
+      a.click();
+    };
+  })
